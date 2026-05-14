b3c08e1fcf83866039a5791ce440899b9ed946f9
Miniks <miniksnhc456@gmail.com>
2026-05-14 09:54:04 +0700

    create project

---
+++ backend/SEAL Hackathon Management System/src/main/java/com/t7/seal/SealHackathonManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.t7.seal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SealHackathonManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SealHackathonManagementSystemApplication.class, args);
+    }
+
+}
---
+++ backend/SEAL Hackathon Management System/src/test/java/com/t7/seal/SealHackathonManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.t7.seal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SealHackathonManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
