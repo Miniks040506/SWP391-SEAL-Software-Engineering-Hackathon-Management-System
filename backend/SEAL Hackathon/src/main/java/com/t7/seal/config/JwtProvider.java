0116ec353895b9a464ccae55990d772a4357445c
Miniks <miniksnhc456@gmail.com>
2026-05-16 11:33:53 +0700

    Add security config

--- backend/SEAL Hackathon/src/main/java/com/t7/seal/config/JwtProvider.java
+++ backend/SEAL Hackathon/src/main/java/com/t7/seal/config/JwtProvider.java
@@ -1,7 +1,58 @@
 package com.t7.seal.config;
 
-import org.springframework.stereotype.Component;
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.security.Keys;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.stereotype.Service;
 
-@Component
+import javax.crypto.SecretKey;
+import java.util.Collection;
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Service
 public class JwtProvider {
+
+    SecretKey key = Keys.hmacShaKeyFor(new JwtConstant().getSecret().getBytes());
+
+    public String generateToken(Authentication authentication) {
+
+        Collection<? extends GrantedAuthority> authorities = authentication.getAuthorities();
+        String roles = populateAuthorities(authorities);
+
+        //86400000 = 24h
+        String jwt = Jwts.builder().setIssuedAt(new Date())
+                .setExpiration(new Date(new Date().getTime() + 24*60*60*1000))
+                .claim("email", authentication.getName())
+                .claim("authorities", roles)
+                .signWith(key).compact();
+
+        return jwt;
+    }
+
+    public String getEmailFromJwtToken(String jwtToken) {
+
+        jwtToken = jwtToken.substring(7);
+
+        Claims claims = Jwts.parserBuilder().setSigningKey(key)
+                .build().parseClaimsJws(jwtToken).getBody();
+        String email = String.valueOf(claims.get("email"));
+
+        return email;
+    }
+
+    private String populateAuthorities(Collection<? extends GrantedAuthority> authorities) {
+
+        Set<String> auths = new HashSet<>();
+
+        for (GrantedAuthority grantedAuthority : authorities) {
+            auths.add(grantedAuthority.getAuthority());
+        }
+
+        return String.join(",", auths);
+    }
+
 }
