3dd61a40675feecfa76c33d9a409494904c5be4a
DatIT-026 <tiendatha2006@gmail.com>
2026-05-27 12:16:36 +0700

    added coordinator-edit-event-page

--- frontend/Seal_Hackathon/src/app/router.tsx
+++ frontend/Seal_Hackathon/src/app/router.tsx
@@ -4,7 +4,7 @@ import { EventsPage, EventDetailPage } from "@/features/events";
 import { LeaderboardPage } from "@/features/ranking";
 import { NotFoundPage } from "@/components/common/NotFoundPage";
 
-import { CoordinatorCreateEventPage, CoordinatorDashboardPage, CoordinatorEventsPage } from "@/features/coordinator";
+import { CoordinatorCreateEventPage, CoordinatorDashboardPage, CoordinatorEventsPage, CoordinatorEditEventPage } from "@/features/coordinator";
 
 import { RegisterPage } from "@/features/auth/pages/RegisterPage";
 import { VerifyEmailPage } from "@/features/auth/pages/VerifyEmailPage";
@@ -51,6 +51,7 @@ export const router = createBrowserRouter([
       { path: 'dashboard', element: <CoordinatorDashboardPage /> },
       { path: 'events', element: <CoordinatorEventsPage /> },
       { path: 'events/create', element: <CoordinatorCreateEventPage /> },
+      { path: 'events/:id/edit', element: <CoordinatorEditEventPage /> },
       { path: 'teams', element: <NotFoundPage /> },
       { path: 'submissions', element: <NotFoundPage /> },
       { path: 'judging', element: <NotFoundPage /> },
