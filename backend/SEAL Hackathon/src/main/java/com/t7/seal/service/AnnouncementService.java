1155511a420dada4af8bf944e787ac4c3b4929f9
Miniks <miniksnhc456@gmail.com>
2026-06-09 01:01:01 +0700

    Add service interface method for announcement

--- backend/SEAL Hackathon/src/main/java/com/t7/seal/service/AnnouncementService.java
+++ backend/SEAL Hackathon/src/main/java/com/t7/seal/service/AnnouncementService.java
@@ -1,6 +1,9 @@
 package com.t7.seal.service;
 
+import com.t7.seal.request.system.CreateAnnouncementRequest;
+import com.t7.seal.request.system.UpdateAnnouncementRequest;
 import com.t7.seal.response.system.AnnouncementResponse;
+import org.springframework.security.core.Authentication;
 
 import java.util.List;
 import java.util.UUID;
@@ -8,5 +11,27 @@ import java.util.UUID;
 public interface AnnouncementService {
     List<AnnouncementResponse> getEventAnnouncements(UUID eventId);
 
+    List<AnnouncementResponse> getManageEventAnnouncements(UUID eventId, Authentication authentication);
+
+    AnnouncementResponse createAnnouncement(UUID eventId, CreateAnnouncementRequest request, Authentication authentication);
+
     AnnouncementResponse getAnnouncementById(UUID announcementId);
+
+    AnnouncementResponse getAnnouncementByIdForManage(UUID announcementId, Authentication authentication);
+
+    AnnouncementResponse updateAnnouncement(UUID announcementId, UpdateAnnouncementRequest request, Authentication authentication);
+
+    void deleteAnnouncement(UUID announcementId, Authentication authentication);
+
+    AnnouncementResponse publishAnnouncement(UUID announcementId, Authentication authentication);
+
+    AnnouncementResponse scheduleAnnouncement(UUID announcementId, UpdateAnnouncementRequest request, Authentication authentication);
+
+    AnnouncementResponse unpublishAnnouncement(UUID announcementId, Authentication authentication);
+
+    AnnouncementResponse pinAnnouncement(UUID announcementId, Authentication authentication);
+
+    AnnouncementResponse unpinAnnouncement(UUID announcementId, Authentication authentication);
+
+    AnnouncementResponse markResultAnnouncement(UUID announcementId, Authentication authentication);
 }
--- backend/SEAL Hackathon/src/main/java/com/t7/seal/service/impl/AnnouncementServiceImpl.java
+++ backend/SEAL Hackathon/src/main/java/com/t7/seal/service/impl/AnnouncementServiceImpl.java
@@ -3,9 +3,12 @@ package com.t7.seal.service.impl;
 import com.t7.seal.entities.EventAnnouncement;
 import com.t7.seal.exception.NotFoundException;
 import com.t7.seal.repository.EventAnnouncementRepository;
+import com.t7.seal.request.system.CreateAnnouncementRequest;
+import com.t7.seal.request.system.UpdateAnnouncementRequest;
 import com.t7.seal.response.system.AnnouncementResponse;
 import com.t7.seal.service.AnnouncementService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -29,11 +32,67 @@ public class AnnouncementServiceImpl implements AnnouncementService {
 
     @Transactional(readOnly = true)
     @Override
+    public List<AnnouncementResponse> getManageEventAnnouncements(UUID eventId, Authentication authentication) {
+        return null;
+    }
+
+    @Override
+    public AnnouncementResponse createAnnouncement(UUID eventId, CreateAnnouncementRequest request, Authentication authentication) {
+        return null;
+    }
+
+    @Transactional(readOnly = true)
+    @Override
     public AnnouncementResponse getAnnouncementById(UUID announcementId) {
         EventAnnouncement ea = eventAnnouncementRepository.findPublicById(announcementId)
                 .orElseThrow(() -> new NotFoundException("Announcement not found " + announcementId));
 
         return toAnnouncementResponse(ea);
+    }
+
+    @Override
+    public AnnouncementResponse getAnnouncementByIdForManage(UUID announcementId, Authentication authentication) {
+        return null;
+    }
+
+    @Override
+    public AnnouncementResponse updateAnnouncement(UUID announcementId, UpdateAnnouncementRequest request, Authentication authentication) {
+        return null;
+    }
+
+    @Override
+    public void deleteAnnouncement(UUID announcementId, Authentication authentication) {
+
+    }
+
+    @Override
+    public AnnouncementResponse publishAnnouncement(UUID announcementId, Authentication authentication) {
+        return null;
+    }
+
+    @Override
+    public AnnouncementResponse scheduleAnnouncement(UUID announcementId, UpdateAnnouncementRequest request, Authentication authentication) {
+        return null;
+    }
+
+    @Override
+    public AnnouncementResponse unpublishAnnouncement(UUID announcementId, Authentication authentication) {
+        return null;
+    }
+
+    @Override
+    public AnnouncementResponse pinAnnouncement(UUID announcementId, Authentication authentication) {
+        return null;
+    }
+
+    @Override
+    public AnnouncementResponse unpinAnnouncement(UUID announcementId, Authentication authentication) {
+        return null;
+    }
+
+    @Override
+    public AnnouncementResponse markResultAnnouncement(UUID announcementId, Authentication authentication) {
+        return null;
     }
 
     //HELPERS
