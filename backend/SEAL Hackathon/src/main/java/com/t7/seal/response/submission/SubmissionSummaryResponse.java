84cbbfa3ab38d667b7349d9906d464d799a39efe
Miniks <miniksnhc456@gmail.com>
2026-06-09 18:34:31 +0700

    Update submission's req and res record

--- backend/SEAL Hackathon/src/main/java/com/t7/seal/response/submission/SubmissionSummaryResponse.java
+++ backend/SEAL Hackathon/src/main/java/com/t7/seal/response/submission/SubmissionSummaryResponse.java
@@ -4,7 +4,17 @@ import java.time.LocalDateTime;
 import java.util.UUID;
 
 public record SubmissionSummaryResponse(
-        UUID id, UUID roundId,
-        String roundName, String status, Integer submissionNumber,
-        LocalDateTime submittedAt)
-{}
+        UUID id,
+        UUID teamId,
+        String teamName,
+        UUID trackId,
+        String trackName,
+        UUID roundId,
+        String roundName,
+        String status,
+        Integer submissionNumber,
+        LocalDateTime submittedAt,
+        LocalDateTime updatedAt,
+        long linkCount
+) {
+}
