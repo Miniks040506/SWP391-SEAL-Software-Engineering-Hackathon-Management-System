133f164499b45a432c7f3bdbd6977116882bf9ad
nguyen2312-dev <khoinguyenho2006@gmail.com>
2026-06-09 23:47:38 +0700

    add prototypes for mentorFeedbackService methods

---
+++ backend/SEAL Hackathon/src/main/java/com/t7/seal/service/MentorFeedbackService.java
@@ -0,0 +1,28 @@
+package com.t7.seal.service;
+
+import com.t7.seal.request.mentor.CreateMentorFeedbackRequest;
+import com.t7.seal.response.mentor.MentorFeedbackResponse;
+import org.springframework.security.core.Authentication;
+
+import java.util.List;
+import java.util.UUID;
+
+public interface MentorFeedbackService {
+    MentorFeedbackResponse createFeedback(UUID teamId,
+                                          CreateMentorFeedbackRequest request,
+                                          Authentication authentication);
+
+    List<MentorFeedbackResponse> getTeamFeedback(UUID teamId, Authentication authentication);
+
+    List<MentorFeedbackResponse> getMentorTeamFeedback(UUID teamId, Authentication authentication);
+
+    MentorFeedbackResponse getMentorFeedbackById(UUID feedbackId, Authentication authentication);
+
+    MentorFeedbackResponse updateFeedback(UUID feedbackId,
+                                          CreateMentorFeedbackRequest request,
+                                          Authentication authentication);
+
+    void deleteFeedback(UUID feedbackId, Authentication authentication);
+
+    MentorFeedbackResponse publishFeedback(UUID feedbackId, Authentication authentication);
+}
