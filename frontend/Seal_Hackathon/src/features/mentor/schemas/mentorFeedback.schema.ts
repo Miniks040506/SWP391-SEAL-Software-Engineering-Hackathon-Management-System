8851b097c8716d029f16e32128634a821277dd54
VoNMThu <vnmthu27@gmail.com>
2026-06-13 11:29:50 +0700

    create files + do schema

---
+++ frontend/Seal_Hackathon/src/features/mentor/schemas/mentorFeedback.schema.ts
@@ -0,0 +1,11 @@
+import { submissionApi } from "@/api";
+import { z } from "zod";
+
+export const mentorFeedbackSchema = z.object({
+    category: z.string().min(1, "Please select a category"),
+    content: z.string().min(10, "Feedback content must be at least 10 characters long"),
+    submissionId: z.string().optional().nullable();
+    roundId: z.string().optional().nullable(),
+});
+
+export type MentorFeedbackFormValues = z.infer<typeof mentorFeedbackSchema>;
