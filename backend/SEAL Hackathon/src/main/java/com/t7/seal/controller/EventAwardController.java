d63b4e69705df437125b2f2d9f30e90d0787fa0b
nguyen2312-dev <khoinguyenho2006@gmail.com>
2026-06-30 14:08:02 +0700

    new controller

---
+++ backend/SEAL Hackathon/src/main/java/com/t7/seal/controller/EventAwardController.java
@@ -0,0 +1,12 @@
+package com.t7.seal.controller;
+
+import com.t7.seal.config.ApiPaths;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping(ApiPaths.API_V1)
+public class EventAwardController {
+}
