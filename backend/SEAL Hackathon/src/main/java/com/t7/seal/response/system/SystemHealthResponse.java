3acf3a769e01a98bc99769f1e2757f8bc7845038
Miniks <miniksnhc456@gmail.com>
2026-07-05 10:11:00 +0700

    Update system config response and add new response for reminder

--- backend/SEAL Hackathon/src/main/java/com/t7/seal/response/system/SystemHealthResponse.java
+++ backend/SEAL Hackathon/src/main/java/com/t7/seal/response/system/SystemHealthResponse.java
@@ -3,7 +3,9 @@ package com.t7.seal.response.system;
 import java.util.Map;
 
 public record SystemHealthResponse(
-        String status, boolean databaseUp,
-        boolean mailUp, boolean storageUp,
+        String status,
+        boolean databaseUp,
+        boolean mailUp,
+        boolean storageUp,
         Map<String, Object> details
 ) {}
