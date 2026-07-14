7f0979768d3ecd65695b0be7f7cdb656cc1d9ade
Miniks <miniksnhc456@gmail.com>
2026-07-14 23:05:56 +0700

    add openAPI request dto schema for results: ClearPrizeAwardRequest, CreateDisqualificationRequest

--- backend/SEAL Hackathon/src/main/java/com/t7/seal/request/results/ClearPrizeAwardRequest.java
+++ backend/SEAL Hackathon/src/main/java/com/t7/seal/request/results/ClearPrizeAwardRequest.java
@@ -1,3 +1,12 @@
 package com.t7.seal.request.results;
 
-public record ClearPrizeAwardRequest(String reason) {}
+import io.swagger.v3.oas.annotations.media.Schema;
+
+@Schema(name = "ClearPrizeAwardRequest", description = "Request payload for clear prize award.")
+public record ClearPrizeAwardRequest(
+        @Schema(
+                description = "Reason supplied for the operation.",
+                example = "Valid reason for the operation."
+        )
+        String reason
+) {}
